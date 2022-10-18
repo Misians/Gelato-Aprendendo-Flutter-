ff066630cf9763ec45b39b9ffc08cc469b52cd51
Filipe Lukebana <filipelukebana@gmail.com>
2022-10-18 13:41:07 -0300

    🔥 Created Icecream Model and Controller

---
+++ gelato-rest-api/app/Http/Controllers/IcecreamController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Icecream;
+use Illuminate\Http\Request;
+
+class IcecreamController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Icecream  $icecream
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Icecream $icecream)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Icecream  $icecream
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Icecream $icecream)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Icecream  $icecream
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Icecream $icecream)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Icecream  $icecream
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Icecream $icecream)
+    {
+        //
+    }
+}
